85c1c3da2802091d59efb24cc9cfe7f7b357a372
cyrilforrez <cyilforrez@gmail.com>
2017-07-07 14:12:36 +0200

    Adds function to hide images

--- js/main.js
+++ js/main.js
@@ -4,66 +4,115 @@ var Player;
 var Computer;
 var scoreP = 0;
 var scoreC = 0;
+var temp;
+var image1 = document.getElementById('ciseaux');
+var image2 = document.getElementById('pierre');
+var image3 = document.getElementById('feuille');
+var image4 = document.getElementById('scissors');
+var image5 = document.getElementById('stone');
+var image6 = document.getElementById('leaf');
 
-//-----------------------------------Main loop---------------------------------------------
+//-----------------------------------prompt name---------------------------------------------
 
 Player = prompt("Veuillez entrer votre nom!");
 
-	document.getElementById("compteur").innerHTML = Player +": "+ scoreP +" "+"-"+" "+"Computer:" +" "+ scoreC;    
-	    
+document.getElementById("compteur").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "Computer:" + " " + scoreC;
 
-	
-
-	
 
 
 //------------------------------------------Main function---------------------------------------
 
-	function comparer(choix) {
-	    var temp;
-		
-		Computer = Math.random();
-	    if (Computer < 0.34) {
-			Computer = "pierre";
-		} else if(Computer <= 0.67) {
-			Computer = "feuille";
-		} else {
-			Computer = "ciseaux";
-		}
+function comparer(choix) {
 
-	    if (choix == Computer) {
-	        return "Egalité !";
-	    }
-	     else if (choix == "pierre") {
-	        if(Computer == "ciseaux") {
-	            temp = 1;
-	            scoreP++;
-	        }
-	        else {
-	            temp =  0;
-	            scoreC++;
-	        }
-	}
-	    else if (choix =="feuille") {
-	        if (Computer =="pierre") {
-	            temp = 1;
-	            scoreP++;
-	        }
-	        else {
-	            temp = 0;
-	            scoreC++;
-	        }
-	    }
-	    else if (choix =="ciseaux") {
-	        if (Computer =="feuille") {
-	            temp = 1;
-	            scoreP++;
-	        }
-	        if (Computer =="pierre") {
-	            temp = 0;
-	            scoreC++;
-	        }
-	    }
 
-	    console.log(choix,Computer); 
-	} 
+
+    Computer = Math.random();
+    if (Computer <= 0.34) {
+        Computer = "pierre";
+    } else if (Computer <= 0.67) {
+        Computer = "feuille";
+    } else {
+        Computer = "ciseaux";
+    }
+
+    image1.onclick = function() {
+        image2.style.display = 'none';
+        image3.style.display = 'none';
+
+        if (Computer == "pierre") {
+            image4.style.display = 'none';
+            image6.style.display = 'none';
+        } else if (Computer == "ciseaux") {
+            image5.style.display = 'none';
+            image6.style.display = 'none';
+        } else {
+            image4.style.display = 'none';
+            image5.style.display = 'none';
+        }
+    };
+
+    image2.onclick = function() {
+        image1.style.display = 'none';
+        image3.style.display = 'none';
+
+        if (Computer == "pierre") {
+            image4.style.display = 'none';
+            image6.style.display = 'none';
+        } else if (Computer == "ciseaux") {
+            image5.style.display = 'none';
+            image6.style.display = 'none';
+        } else {
+            image4.style.display = 'none';
+            image5.style.display = 'none';
+        }
+    };
+
+    image3.onclick = function() {
+        image1.style.display = 'none';
+        image2.style.display = 'none';
+
+        if (Computer == "pierre") {
+            image4.style.display = 'none';
+            image6.style.display = 'none';
+        } else if (Computer == "ciseaux") {
+            image5.style.display = 'none';
+            image6.style.display = 'none';
+        } else {
+            image4.style.display = 'none';
+            image5.style.display = 'none';
+        }
+    };
+
+
+    if (choix == Computer) {
+        return "Egalité !";
+    } else if (choix == "pierre") {
+        if (Computer == "ciseaux") {
+            temp = 1;
+            scoreP++;
+        } else {
+            temp = 0;
+            scoreC++;
+        }
+    } else if (choix == "feuille") {
+        if (Computer == "pierre") {
+            temp = 1;
+            scoreP++;
+        } else {
+            temp = 0;
+            scoreC++;
+        }
+    } else if (choix == "ciseaux") {
+        if (Computer == "feuille") {
+            temp = 1;
+            scoreP++;
+        }
+        if (Computer == "pierre") {
+            temp = 0;
+            scoreC++;
+        }
+    }
+
+    document.getElementById("compteur").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "Computer:" + " " + scoreC;
+
+}
