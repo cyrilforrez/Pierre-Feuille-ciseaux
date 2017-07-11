31771be3264a3b39f8c9a154e2e249304f12af57
cyrilforrez <cyilforrez@gmail.com>
2017-07-11 11:36:27 +0200

    add exit option

--- js/main.js
+++ js/main.js
@@ -60,49 +60,130 @@ function comparer(choix) {
     //------------------------------------------Compares the different choices-------------------------------------
     if (choix == Computer) {
 
-        return "Egalité !";
+        alert("Egalité !");
+        image1.style.display = '';
+        image2.style.display = '';
+        image3.style.display = '';
+        image4.style.display = '';
+        image5.style.display = '';
+        image6.style.display = '';
 
 
     } else if (choix == "pierre") {
         if (Computer == "ciseaux") {
             temp = 1;
             scoreP++;
-            document.getElementById("player").style.animation = "mymove 5s";
+            document.getElementById("player").style.animation = "mymove 3s";
             document.getElementById("player").style.zIndex = "1";
-            
+            document.getElementById("player").style.animationFillMode = "forwards";
+            alert("Vous avez gagné!");
+            document.getElementById("player").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
+
         } else {
             temp = 0;
             scoreC++;
-            document.getElementById("computer").style.animation = "mymove2 5s";
+            document.getElementById("computer").style.animation = "mymove2 3s";
+            document.getElementById("computer").style.animationFillMode = "forwards";
+            alert("Vous avez perdu!");
+            document.getElementById("computer").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
         }
     } else if (choix == "feuille") {
         if (Computer == "pierre") {
             temp = 1;
             scoreP++;
-            document.getElementById("player").style.animation = "mymove 5s";
+            document.getElementById("player").style.animation = "mymove 3s";
             document.getElementById("player").style.zIndex = "1";
+            document.getElementById("player").style.animationFillMode = "forwards";
+            alert("Vous avez gagné!");
+            document.getElementById("player").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
         } else {
             temp = 0;
             scoreC++;
-            document.getElementById("computer").style.animation = "mymove2 5s";
+            document.getElementById("computer").style.animation = "mymove2 3s";
+            document.getElementById("computer").style.animationFillMode = "forwards";
+            alert("Vous avez perdu!");
+            document.getElementById("computer").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
 
         }
     } else if (choix == "ciseaux") {
         if (Computer == "feuille") {
             temp = 1;
             scoreP++;
-            document.getElementById("player").style.animation = "mymove 5s";
+            document.getElementById("player").style.animation = "mymove 3s";
             document.getElementById("player").style.zIndex = "1";
+            document.getElementById("player").style.animationFillMode = "forwards";
+            alert("Vous avez gagné!");
+            document.getElementById("player").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
         }
         if (Computer == "pierre") {
             temp = 0;
             scoreC++;
-            document.getElementById("computer").style.animation = "mymove2 5s";
+            document.getElementById("computer").style.animation = "mymove2 3s";
+            document.getElementById("computer").style.animationFillMode = "forwards";
+            alert("Vous avez perdu!");
+            document.getElementById("computer").style.animationFillMode = "backwards";
+            image1.style.display = '';
+            image2.style.display = '';
+            image3.style.display = '';
+            image4.style.display = '';
+            image5.style.display = '';
+            image6.style.display = '';
         }
     }
 
-
     //---------------------------------------------------Table score-------------------------------------------------
-    document.getElementById("compteur").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "Computer:" + " " + scoreC;
-    document.getElementById("score").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "C:" + " " + scoreC;
+    document.getElementById("compteur").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "Computer" + ":" + " " + scoreC;
+    document.getElementById("score").innerHTML = Player + ": " + scoreP + " " + "-" + " " + "C" + ":" + " " + scoreC;
+
+    if ((scoreP) == 3) {
+        alert("Vous avez gagné!" + " " + Player + ": " + scoreP + " " + "-" + " " + "Computer" + ":" + " " + scoreC);
+        confirm("Voulez vous rejouer?");
+        if (confirm == true) {
+            window.location.reload();
+        } else {
+            document.getElementById("end").style.display = 'block';
+            document.getElementById("end").style.zIndex = '1';
+        }
+    }
+    else if ((scoreC) == 3)  {
+        alert("Vous avez perdu!" + " " + Player + ": " + scoreP + " " + "-" + " " + "Computer" + ":" + " " + scoreC);
+        confirm("Voulez vous rejouer?");
+        if (confirm == true) {
+            window.location.reload();
+        } else {
+            document.getElementById("end").style.display = 'block';
+            document.getElementById("end").style.zIndex = '1';
+        }
+    }
+
 }
